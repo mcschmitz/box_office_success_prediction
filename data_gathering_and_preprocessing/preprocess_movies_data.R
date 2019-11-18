ada340de53c6517073ef65b2872b36d45b12b015
Manuel Schmitz <manuel.schmitz@gmx.net>
2019-11-18 17:07:28 +0100

    add raw data

--- data_gathering_and_preprocessing/preprocess_movies_data.R
+++ data_gathering_and_preprocessing/preprocess_movies_data.R
@@ -1,19 +1,11 @@
 source("utils/preprocessing_utils.R")
 
 movies = read.csv("data/movies.csv", sep = ";", dec = ",", comment.char = "", encoding = "UTF-8", na.strings = "")
-names(movies) = c("Kinostart", "Filmtitel", "Verleiher", "Kinderfilm", "FSK", "Genre_Prognose", "Genre1", "Genre2",
-                  "Kopien", "Besucher_wochenende1", "Kopienschnitt", "Besucher_gesamt", "Tipp_min6", "Tipp_max6",
-                  "Tipp_min5","Tipp_max5", "Tipp_min4", "Tipp_max4", "Tipp_min3","Tipp_max3", "Tipp_min2", "Tipp_max2",
-                  "Tipp_min1", "Tipp_max1", "Tippspiel", "Awareness_unaided6", "Awareness_unaided5",
-                  "Awareness_unaided4", "Awareness_unaided3", "Awareness_unaided2", "Awareness_unaided1",
-                  "Awareness_aided6", "Awareness_aided5", "Awareness_aided4", "Awareness_aided3", "Awareness_aided2",
-                  "Awareness_aided1", "First_choice6", "First_choice5", "First_choice4", "First_choice3", 
-                  "First_choice2", "First_choice1")
-
-movies$Filmtitel = as.character(movies$Filmtitel)
-movies$FSK = as.factor(movies$FSK)
-movies$Kinostart = as.POSIXct(movies$Kinostart, format = "%d.%m.%y", tz = "GMT")
-movies$Kinderfilm = as.factor(ifelse(test = movies$Kinderfilm == "ja", yes = 1, no = 0))
+movies$premiere_date = as.POSIXct(movies$premiere_date, format = "%d.%m.%y", tz = "GMT")
+movies$title = as.character(movies$title)
+movies$genre = as.character(movies$genre)
+movies$rating = as.factor(movies$rating)
+movies$kids_movie = as.factor(ifelse(test = movies$kids_movie == "yes", yes = 1, no = 0))
 
 # Add studio category
 major = c("20th Century Fox", "Paramount Pictures", "Sony Pictures Releasing", "Universal Pictures", "Walt Disney",
@@ -25,62 +17,62 @@ independent = c("Alamode Film", "Alpenrepublik", "Arsenal (Central)", "Arts Alli
                 "KSM GmbH", "Maerchenfilm", "MFA+ Filmdistribution", "Movienet", "Neue Visionen", "NFP", "Pandastorm",
                 "Pandora", "polyband", "Port-au-Prince", "Pro-Fun", "Prokino", "Splendid", "SpotOn Distribution",
                 "Studio Hamburg", "Summiteer Films","Tiberius Film", "Weltkino", "Zorro")
-movies$Verleiherkategorie = as.factor(ifelse(movies$Verleiher %in% major, "major",
-                                             ifelse(movies$Verleiher %in% major_independent, 
-                                                    "major_independent", "independent")))
+movies$studio = as.factor(ifelse(movies$studio %in% major, "major",
+                                 ifelse(movies$studio %in% major_independent, "major_independent", "independent")))
 
-movies$Besucher_wochenende1_log = log(movies$Besucher_wochenende1)
-movies$Besucher_gesamt_log = log(movies$Besucher_gesamt)
+movies$visitors_premiere_weekend_log = log(movies$visitors_premiere_weekend)
+movies$visitors_overall_log = log(movies$visitors_overall)
 
 # Add forecast horizon dates
-movies$Prognose_start = do.call(what = c, args = lapply(X = movies$Kinostart, FUN = forecast_date, forecast = 7)) + 
+movies$forecast_start = do.call(what = c, args = lapply(X = movies$premiere_date, FUN = forecast_date, forecast = 7)) + 
   as.difftime(tim = 1, units = "days")
-movies$Prognosedatum6 = do.call(what = c, args = lapply(X = movies$Kinostart, FUN = forecast_date, forecast = 6))
-movies$Prognosedatum5 = do.call(what = c, args = lapply(X = movies$Kinostart, FUN = forecast_date, forecast = 5))
-movies$Prognosedatum4 = do.call(what = c, args = lapply(X = movies$Kinostart, FUN = forecast_date, forecast = 4))
-movies$Prognosedatum3 = do.call(what = c, args = lapply(X = movies$Kinostart, FUN = forecast_date, forecast = 3))
-movies$Prognosedatum2 = do.call(what = c, args = lapply(X = movies$Kinostart, FUN = forecast_date, forecast = 2))
-movies$Prognosedatum1 = do.call(what = c, args = lapply(X = movies$Kinostart, FUN = forecast_date, forecast = 1))
+movies$forecast_date6 = do.call(what = c, args = lapply(X = movies$premiere_date, FUN = forecast_date, forecast = 6))
+movies$forecast_date5 = do.call(what = c, args = lapply(X = movies$premiere_date, FUN = forecast_date, forecast = 5))
+movies$forecast_date4 = do.call(what = c, args = lapply(X = movies$premiere_date, FUN = forecast_date, forecast = 4))
+movies$forecast_date3 = do.call(what = c, args = lapply(X = movies$premiere_date, FUN = forecast_date, forecast = 3))
+movies$forecast_date2 = do.call(what = c, args = lapply(X = movies$premiere_date, FUN = forecast_date, forecast = 2))
+movies$forecast_date1 = do.call(what = c, args = lapply(X = movies$premiere_date, FUN = forecast_date, forecast = 1))
 
 # Replace "&" with "und" for german movies and "and" for english ones
-movies$Filmtitel[c(67, 95, 151, 236, 559, 608, 621, 826, 836, 846)] = 
-  gsub(movies$Filmtitel[c(67, 95, 151, 236, 559, 608, 621, 826, 836, 846)], pattern = "&", replacement = "and")
-movies$Filmtitel[c(84, 97, 280, 286, 293, 486, 506, 727, 771, 894)] = 
-  gsub(movies$Filmtitel[c(84, 97, 280, 286, 293, 486, 506, 727, 771, 894)], pattern = "&", replacement = "und")
+movies$title[c(67, 95, 151, 236, 559, 608, 621, 826, 836, 846)] = 
+  gsub(movies$title[c(67, 95, 151, 236, 559, 608, 621, 826, 836, 846)], pattern = "&", replacement = "and")
+movies$title[c(84, 97, 280, 286, 293, 486, 506, 727, 771, 894)] = 
+  gsub(movies$title[c(84, 97, 280, 286, 293, 486, 506, 727, 771, 894)], pattern = "&", replacement = "und")
 
 # Correct some movvie titles:
-movies$Filmtitel[788] = "Tschiller: Off Duty"
-movies$Filmtitel[572] = "Dessau Dancers"
+movies$title[788] = "Tschiller: Off Duty"
+movies$title[572] = "Dessau Dancers"
 
 # Remove "Eine Taube sitzt auf einem Zweig und denkt über das Leben nach" und "#Zeitgeist" aus dem Datensatz:
 movies = movies[-491, ]
 movies = movies[-479,]
 
 # Get end of forecast horizon
-end_forecast_week = do.call(what = c, args = lapply(X = movies$Kinostart, FUN = forecast_date, forecast = 1))
-end_forecast_week = trunc(x = end_forecast_week, units = "days")
+last_forecast_date = do.call(what = c, args = lapply(X = movies$premiere_date, FUN = forecast_date, forecast = 1))
+last_forecast_date = trunc(x = last_forecast_date, units = "days")
 
 # Define searchterms for Google Trends
-main_title = define_searchterms(movies$Filmtitel)[[1]]
-main_title_film = define_searchterms(movies$Filmtitel)[[2]]
-complete_title = define_searchterms(movies$Filmtitel)[[3]]
-movies$Haupttitel = main_title
-movies$Untertitel = as.factor(ifelse(test = main_title == complete_title, yes = 0, no = 1))
+main_title = define_searchterms(movies$title)[[1]]
+main_title_film = define_searchterms(movies$title)[[2]]
+complete_title = define_searchterms(movies$title)[[3]]
+movies$main_title = main_title
+movies$secondary_title = as.factor(ifelse(test = main_title == complete_title, yes = 0, no = 1))
 
-movies$Woerter = sapply(X = strsplit(x = movies$Haupttitel, split = " "), FUN = length)
-movies$Monat = factor(strftime(movies$Kinostart, format = "%b"))
-movies$Monat = factor(movies$Monat, levels = unique(movies$Monat))
+movies$words = sapply(X = strsplit(x = movies$main_title, split = " "), FUN = length)
+movies$month = factor(strftime(movies$premiere_date, format = "%b"))
+movies$month = factor(movies$month, levels = unique(movies$month))
 
-movies$Saison = as.factor(ifelse(test = movies$Monat %in% c("Okt", "Nov", "Dez", "Jan", "Feb", "Mär"), 
-                                 yes = "Winter", no = "Sommer"))
-movies$Jahr_gerade = as.numeric(strftime(x = movies$Kinostart, format = "%y"))
-movies$Jahr_gerade = as.factor(ifelse(test = movies$Jahr_gerade %% 2 == 0, yes = 1, no = 0))
+movies$season = as.factor(ifelse(test = movies$month %in% c("Okt", "Nov", "Dez", "Jan", "Feb", "Mär"), 
+                                 yes = "winter", no = "summer"))
+movies$year_even = as.numeric(strftime(x = movies$premiere_date, format = "%y"))
+movies$odd_year = as.factor(ifelse(test = movies$year_even %% 2 == 0, yes = 1, no = 0))
 
 # Concatenate genres "Fantasy" and "Teenager-Film":
-levels(movies$Genre_Prognose) = c(levels(movies$Genre_Prognose), "Teenager-Film/Fantasy")
-movies$Genre_Prognose[movies$Genre_Prognose == "Fantasy"] = "Teenager-Film/Fantasy"
-movies$Genre_Prognose[movies$Genre_Prognose == "Teenager-Film"] = "Teenager-Film/Fantasy"
-movies$Genre_Prognose = droplevels(movies$Genre_Prognose)
+movies$genre[movies$genre == "Fantasy"] = "TeenagerMovie_Fantasy"
+movies$genre[movies$genre == "TeenagerMovie"] = "TeenagerMovie_Fantasy"
+movies$genre = as.factor(movies$genre)
 
-remove(major, major_independent, independent, forecast_date, define_searchterms)
+remove(major, major_independent, independent, forecast_date, calculate_box_cox,
+       calculate_google_value, calculate_median, cross_validate_inner, define_searchterms, estimate_box_cox,
+       optim_weights, select_gt_data)
 save.image("data/movies_preprocessed.RData")
